92d5ae053f936d89693679e97ce3b4c747a58b87
Nouran Elshazly <nouranelshazly19@gmail.com>
2026-03-04 01:35:41 +0300

    add screens

--- event_booking/lib/main.dart
+++ event_booking/lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:event_booking/features/profile/events_screen.dart';
-import 'package:event_booking/features/search/pages/search_white_bar.dart';
+// import 'package:event_booking/features/profile/events_screen.dart';
+// import 'package:event_booking/features/search/pages/search_white_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:event_booking/core/styles/apptheme.dart';
-import 'package:event_booking/features/welcom/pages/splash_screen.dart';
+// import 'package:event_booking/features/welcom/pages/splash_screen.dart';
+import 'package:event_booking/features/profile/profile_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -26,7 +27,7 @@ class MainApp extends StatelessWidget {
       },
       debugShowCheckedModeBanner: false,
       // home: SplashScreen(),
-      home: SearchWhiteBar(),
+      home: ProfileScreen(),
     );
   }
 }
